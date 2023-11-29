193aa24cf3b1f9df704e048a59f44bdd8612472a
Ilia Diachenko <150364742+DiaElijah@users.noreply.github.com>
2023-11-29 18:53:31 +0300

    plotapp.py

--- Streamlit_project/plotapp.py
+++ Streamlit_project/plotapp.py
@@ -1,12 +1,16 @@
+!pip install streamlit_echarts
+!pip install plotly
+!pip install scikit-learn
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from random import randint
 from streamlit_echarts import st_echarts
-# from plotly.tools import FigureFactory as ff
-# import plotly.express as px
-# from sklearn.preprocessing import LabelEncoder
+from plotly.tools import FigureFactory as ff
+import plotly.express as px
+from sklearn.preprocessing import LabelEncoder
 
 st.write('''
 # Приложение с невероятными графиками чаевых
